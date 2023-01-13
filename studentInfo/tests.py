d42d43759f2c067614330ee156c4cdfa0104916c
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-01-14 00:04:47 +0330

    added some test for User model in tests.py file

--- studentInfo/tests.py
+++ studentInfo/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class ModelTest(TestCase):
+    
+    def create_user(self, national_code, student_id):
+        return User.objects.create(national_code=national_code,
+                                   student_id=student_id)
+    
+    def test_user_creation(self):
+        valid_national_code = '1111111111'
+        valid_student_id = '9811126101'
+        new_user = self.create_user(valid_national_code, valid_student_id)
+        self.assertTrue(isinstance(new_user, User))
+        self.assertEqual(new_user.__str__(), valid_national_code)
+        
+    def test_invalidNationalCodeLength(self):
+        invalid_national_code = '11111111111'
+        valid_student_id = '9811126101'
+        self.assertRaises(Exception, 
+                          self.create_user, 
+                          invalid_national_code,
+                          valid_student_id)
+    
+    def test_invalidStudentIdLength(self):
+        vaild_national_code = '1111111111'
+        invalid_student_id = '9811126101'
+        self.assertRaises(Exception,
+                          self.create_user,
+                          vaild_national_code,
+                          invalid_student_id)        
